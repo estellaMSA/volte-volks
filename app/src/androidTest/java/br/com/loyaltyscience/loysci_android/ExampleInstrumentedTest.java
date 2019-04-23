f1d7d4b86cbe84597a5657ff20041b375fc60c35
Estella Menna <emenna@loysci.com>
2019-04-23 12:00:01 -0300

    Start Commit Volte Volks

--- app/src/androidTest/java/br/com/loyaltyscience/loysci_android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/br/com/loyaltyscience/loysci_android/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("br.com.monitoratec.loysci_android", appContext.getPackageName());
+        assertEquals("br.com.loyaltyscience.loysci_android", appContext.getPackageName());
     }
 }
